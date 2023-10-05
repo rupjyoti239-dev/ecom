33f7a68eac6c2792c22a0b818ce3ee63692ce18e
Rupjyoti Sarma <rup.sarma23@gmail.com>
2023-10-05 22:05:34 +0530

    new feature: search, filter and image gallery

--- app/index.js
+++ app/index.js
@@ -13,3 +13,112 @@ if(closeBtn){
     nav.classList.remove('active')
   })
 }
+
+
+
+
+
+
+// product search
+function search(){
+  let filter = document.getElementById('myInput').value.toUpperCase();
+   let productContainer = document.getElementById('products');
+
+  let card = productContainer.children;
+  console.log(card);
+
+
+  for(let i=0;i<card.length;i++){
+    let p = card[i].getElementsByClassName('catagory')[0]
+    console.log(p);
+
+    if(p){
+      let textValue = p.textContent || p.innerHTML;
+
+      if(textValue.toUpperCase().indexOf(filter) >-1){
+          card[i].style.display = "";
+        }else{
+          card[i].style.display = "none";
+        }
+    }
+
+    
+
+  }
+
+}
+
+
+
+
+
+
+// product filter
+(
+  ()=>{
+    const buttons = document.querySelectorAll('.filter-btn')
+    const storeData = document.querySelectorAll('.card')
+
+
+   buttons.forEach((button)=>{
+
+
+    button.addEventListener('click', (e)=>{
+      e.preventDefault();
+      const filter = e.target.dataset.filter;
+
+
+      
+    storeData.forEach((item)=>{
+      if(filter === "all"){
+        item.style.display = "block"
+      }
+      else{
+        if(item.classList.contains(filter)){
+          item.style.display = "block"
+        }
+        else{
+          item.style.display = "none"
+        }
+      }
+
+
+    })
+
+
+
+
+
+    })
+   })
+
+  }
+)()
+
+
+
+
+
+
+
+
+
+
+
+// image gallery
+let mainImg = document.getElementById('main_img');
+let smallImg = document.getElementsByClassName('small_img');
+
+smallImg[0].onclick = function(){
+  mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+  mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+  mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+  mainImg.src = smallImg[3].src;
+}
+
